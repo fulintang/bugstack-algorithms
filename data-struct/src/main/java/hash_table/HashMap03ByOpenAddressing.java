9959d1734da4d005b8d85d4577e0521321048af6
fulin.tang <tangtangdeyx@126.com>
2022-11-21 15:54:36 +0800

    开放寻址

--- data-struct/src/main/java/hash_table/HashMap03ByOpenAddressing.java
+++ data-struct/src/main/java/hash_table/HashMap03ByOpenAddressing.java
@@ -1,20 +1,43 @@
 package hash_table;
 
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 开放寻址
+ *
+ * @param <K>
+ * @param <V>
+ */
 public class HashMap03ByOpenAddressing<K, V> implements Map<K, V> {
-    
+
     private final Logger log = LoggerFactory.getLogger(HashMap03ByOpenAddressing.class);
     private final Node<K, V>[] tab = new Node[8];
-    
+
     @Override
     public void put(K key, V value) {
-        
+        int idx = key.hashCode() & (tab.length - 1);
+        if (tab[idx] == null) {
+            tab[idx] = new Node<>(key, value);
+        } else {
+            for (int i = idx, len = tab.length; i < len; i++) {
+                if (tab[i] == null) {
+                    tab[i] = new Node<>(key, value);
+                    break;
+                }
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        int idx = key.hashCode() & (tab.length - 1);
+        for (int i = idx; i < tab.length; i++) {
+            if (tab[i] != null && tab[i].key == key) {
+                return tab[i].value;
+            }
+        }
         return null;
     }
 
@@ -36,6 +59,12 @@ public class HashMap03ByOpenAddressing<K, V> implements Map<K, V> {
         }
 
     }
-    
-    
+
+    @Override
+    public String toString() {
+        return "HashMap03ByOpenAddressing{" +
+                "tab=" + JSON.toJSONString(tab) +
+                '}';
+    }
+
 }
--- data-struct/src/main/java/hash_table/_test_/HashTableTest.java
+++ data-struct/src/main/java/hash_table/_test_/HashTableTest.java
@@ -2,6 +2,7 @@ package hash_table._test_;
 
 import hash_table.HashMap01;
 import hash_table.HashMap02BySeparateChaining;
+import hash_table.HashMap03ByOpenAddressing;
 import hash_table.Map;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -39,6 +40,24 @@ public class HashTableTest {
         log.info("碰撞后 key：{} value：{}", "01", map.get("01"));
 
 
+    }
+    
+    @Test
+    public void testHashMap03() {
+
+        Map<String, String> map = new HashMap03ByOpenAddressing<>();
+        map.put("01", "花花");
+        map.put("02", "豆豆");
+        log.info("碰撞前 key：{} value：{}", "01", map.get("01"));
+
+        map.put("09", "蛋蛋");
+        map.put("12", "苗苗");
+        log.info("碰撞后 key：{} value：{}", "01", map.get("01"));
+        log.info("碰撞后 key：{} value：{}", "02", map.get("02"));
+        log.info("碰撞后 key：{} value：{}", "09", map.get("09"));
+        log.info("碰撞后 key：{} value：{}", "12", map.get("12"));
+
+        log.info("数据结构：{}", map);
     }
     
 
